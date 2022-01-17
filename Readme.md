f0a598e11b02ee167a0215211c1a66e62c54143b
A-Vaibhav <vaibhav.m.ambekar@gmail.com>
2022-01-18 00:04:17 +0530

    edited readme

--- Readme.md
+++ Readme.md
@@ -1,1 +1,9 @@
 Access Aws Services programmatically using python-boto3
+
+Prerequisite :
+1. Install Aws Cli.
+2. Create User in Aws console with programatic access and download the credentials (Access id & Access key)
+3.Setup Aws cli with user credentials :
+```
+aws configure
+```
---
+++ create_describe_instance.py
@@ -0,0 +1,29 @@
+import boto3
+
+region = "ap-south-1"
+ec2_resource = boto3.resource('ec2', region_name = region)
+ami_id = 'ami-041d6256ed0f2061c'
+
+instances = ec2_resource.create_instances(
+    MinCount = 1,
+    MaxCount = 2,
+    ImageId = ami_id,
+    InstanceType = 't2.micro',
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': 'My-Server'
+                },
+            ]
+        },
+    ]
+)
+
+for instance in instances:
+    print(f'EC2 instance "{instance.id}" has been launched')
+
+    instance.wait_until_running()
+    print(f'EC2 instance "{instance.id}" has been started')
