b2059fd528ab576bfa5cc00b14531f4a1a426130
A-Vaibhav <vaibhav.m.ambekar@gmail.com>
2022-01-18 17:46:34 +0530

    modified

--- list_ebs_volumes.py
+++ list_ebs_volumes.py
@@ -4,12 +4,11 @@ import boto3
 
 region = 'ap-south-1'
 ec2_resource = boto3.resource('ec2', region_name = region)
-instance_id = 'i-0235e6f5e25b5d70b'
+instance_id = 'i-057afa78d84178c05'  # updated the id 
 
 instance_details = ec2_resource.Instance(instance_id)
 
 device_mapped = instance_details.block_device_mappings
-# print(device_mapped)
 
 print(f'Volumes Attached to {instance_id} :')
 for device in device_mapped:
