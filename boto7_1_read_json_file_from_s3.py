e9f4d54e610c7749df68a3748aa5660a3d7693d3
A-Vaibhav <vaibhav.m.ambekar@gmail.com>
2022-01-22 17:18:00 +0530

    read s3 json files

---
+++ boto7_1_read_json_file_from_s3.py
@@ -0,0 +1,20 @@
+import boto3
+import json
+
+s3_resource = boto3.resource('s3')
+
+buckets = s3_resource.buckets.all()
+
+for bucket in buckets:
+    if bucket.name == 'vai-bucket':
+        for object in bucket.objects.all():
+            if object.key[-4:] == 'json':
+               data = object.get()
+               real_data = data['Body'].read()  # in string format
+               jsondict = json.loads(real_data) # converts in dictionary format
+
+
+for key,value in jsondict.items():
+    print(f'{key}:{value}')
+
+               
